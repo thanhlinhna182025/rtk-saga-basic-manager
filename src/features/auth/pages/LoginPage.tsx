2680fe18640c9ae42a10d73da75046311b6054d7
thanhtruong <thanhlinhna182025@gmail.com>
2022-11-09 00:09:16 +0700

    LoginPage module

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,3 +1,14 @@
+import classNames from 'classnames/bind';
+import styles from './LoginPage.module.scss';
+
 export default function LoginPage() {
-  return <div>Login</div>;
+  const cx = classNames.bind(styles);
+
+  return (
+    <div className={cx('wrapper')}>
+      <div className={cx('page')}>
+        <button className={cx('button')}>Login fake</button>
+      </div>
+    </div>
+  );
 }
